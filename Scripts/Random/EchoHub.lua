244902e281b63c4f567704f77fa1a15b97bb75f9
Habibi <habibi0001x@gmail.com>
2025-11-22 18:17:47 +0100

    Create EchoHub.lua

---
+++ Scripts/Random/EchoHub.lua
@@ -0,0 +1,37 @@
+local gamescripts = {
+    [126884695634066] = "https://api.luarmor.net/files/v3/loaders/868384a9798a122db625b53199ae84bb.lua",
+    [2753915549]      = "https://api.luarmor.net/files/v3/loaders/36ee5f40e2b57d44f2af0049d438cf09.lua",
+    [124977557560410] = "https://api.luarmor.net/files/v3/loaders/36ee5f40e2b57d44f2af0049d438cf09.lua",
+    [4442272183]      = "https://api.luarmor.net/files/v3/loaders/36ee5f40e2b57d44f2af0049d438cf09.lua",
+    [101151419317285] = "https://api.luarmor.net/files/v3/loaders/36ee5f40e2b57d44f2af0049d438cf09.lua",
+    [7449423635]      = "https://api.luarmor.net/files/v3/loaders/36ee5f40e2b57d44f2af0049d438cf09.lua",
+    [96342491571673]  = "https://api.luarmor.net/files/v3/loaders/e5f9f615f943858198b7816b963cf706.lua",
+    [109983668079237] = "https://api.luarmor.net/files/v3/loaders/e5f9f615f943858198b7816b963cf706.lua",
+    [79546208627805]  = "https://api.luarmor.net/files/v3/loaders/87684dc664efb78576c425146fcea4e3.lua",
+    [126509999114328] = "https://api.luarmor.net/files/v3/loaders/87684dc664efb78576c425146fcea4e3.lua",
+    [121864768012064] = "https://api.luarmor.net/files/v3/loaders/02d021bc5317adc01b1f42696184ba16.lua",
+    [142823291]       = "https://api.luarmor.net/files/v3/loaders/2110e245e129f6c1d2b451d26dd2712e.lua",
+    [537413528]       = "https://api.luarmor.net/files/v3/loaders/2f521b6701bfd454c0ed67791cc60c85.lua",
+    [127742093697776] = "https://api.luarmor.net/files/v3/loaders/5b8bcc342eb805a81c7a5772b5d5c370.lua",
+    [16732694052]     = "https://api.luarmor.net/files/v3/loaders/ba120508e61c7cd87899ec69bc544788.lua",
+    [75366259315586]  = "https://api.luarmor.net/files/v3/loaders/e7e07fa79015c1a824564319871974f7.lua",
+    [18687417158]     = "https://api.luarmor.net/files/v3/loaders/75e13abad03cb5fd562217824c5ad469.lua",
+    [93978595733734]  = "https://api.luarmor.net/files/v3/loaders/6ebb70c161d6d5aefd9034cc967a85f5.lua",
+    [137925884276740] = "https://api.luarmor.net/files/v3/loaders/ee156e264d4527013c79fda9c6244854.lua"
+}
+
+
+local gameid = game.PlaceId
+
+if gamescripts[gameid] then
+    local url = gamescripts[gameid]
+    loadstring(game:HttpGet(url))()
+else
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Not Supported",
+        Text = "This game isn't supported",
+        Duration = 5
+    })
+end
+
+-- // this is here to debug/update any attempt of deobfuscation/dumping will resault in nothing, since you cannot dump it. lol.
