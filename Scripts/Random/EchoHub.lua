901392d278ac93842399e97ebf06ff337db707b7
Habibi <habibi0001x@gmail.com>
2025-11-28 14:27:58 +0100

    Update EchoHub.lua

--- Scripts/Random/EchoHub.lua
+++ Scripts/Random/EchoHub.lua
@@ -7,17 +7,17 @@ local gamescripts = {
     [7449423635]      = "https://api.luarmor.net/files/v3/loaders/36ee5f40e2b57d44f2af0049d438cf09.lua",
     [96342491571673]  = "https://api.luarmor.net/files/v3/loaders/e5f9f615f943858198b7816b963cf706.lua",
     [109983668079237] = "https://api.luarmor.net/files/v3/loaders/e5f9f615f943858198b7816b963cf706.lua",
-    [79546208627805]  = "https://api.luarmor.net/files/v3/loaders/87684dc664efb78576c425146fcea4e3.lua",
+    [79546208627805]  = "https://api.luarmor.net/files/v3/loaders/af6a9697d1cf7e8a288fb1279b363385.lua",
     [126509999114328] = "https://api.luarmor.net/files/v3/loaders/87684dc664efb78576c425146fcea4e3.lua",
     [121864768012064] = "https://api.luarmor.net/files/v3/loaders/02d021bc5317adc01b1f42696184ba16.lua",
     [142823291]       = "https://api.luarmor.net/files/v3/loaders/2110e245e129f6c1d2b451d26dd2712e.lua",
     [537413528]       = "https://api.luarmor.net/files/v3/loaders/8d16095fa5d1532a6dc727beafdefa61.lua",
     [127742093697776] = "https://api.luarmor.net/files/v3/loaders/5b8bcc342eb805a81c7a5772b5d5c370.lua",
     [16732694052]     = "https://api.luarmor.net/files/v3/loaders/ba120508e61c7cd87899ec69bc544788.lua",
-    [75366259315586]  = "https://api.luarmor.net/files/v3/loaders/e7e07fa79015c1a824564319871974f7.lua",
+    -- // Build ur base removed.
     [18687417158]     = "https://api.luarmor.net/files/v3/loaders/c8bbebed769379dd1b3f8e1840c9d3c2.lua",
-    [93978595733734]  = "https://api.luarmor.net/files/v3/loaders/6ebb70c161d6d5aefd9034cc967a85f5.lua",
-    [137925884276740] = "https://api.luarmor.net/files/v3/loaders/ee156e264d4527013c79fda9c6244854.lua"
+    -- // Violence district removed.
+    -- // Build a Plane removed. 
     
 }
 
