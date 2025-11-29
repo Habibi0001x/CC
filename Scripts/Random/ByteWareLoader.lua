17d417b173b088e7494088742459d496963ee291
Habibi <habibi0001x@gmail.com>
2025-11-29 18:06:46 +0100

    Update ByteWareLoader.lua

--- Scripts/Random/ByteWareLoader.lua
+++ Scripts/Random/ByteWareLoader.lua
@@ -1,1 +1,5 @@
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/67891049c94a9d49f26a2cd769f5bdb1.lua"))() -- sab 
 
+
+
+-- // Note, I am not the owner of this project. This is just a placeholder I had todo.
